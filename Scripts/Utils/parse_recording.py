03f858c76fc6ef03514d391cf63a414982fda0a1
TheSchilk <schilk.philipp@gmail.com>
2020-11-25 11:08:26 +0100

    Documented Code

--- Scripts/Utils/parse_recording.py
+++ Scripts/Utils/parse_recording.py
@@ -2,6 +2,13 @@ import numpy as np
 
 
 def parse_recording_data(data):
+    """
+    Parse a list of raw packages into a numpy array of audio samples, scaled to ADC full-scale
+    :param data: List of samples (int)
+    :return: (audio, fail_count)
+            audio: numpy array of scaled audio samples
+            fail_count: number of samples that could not be decoded
+    """
     (samples, fail_count) = find_samples(data)
 
     audio = np.array(samples, dtype=float)
@@ -16,6 +23,14 @@ def parse_recording_data(data):
 
 
 def find_samples(data):
+    """
+    Parse a list of raw packages into a list of raw samples
+    :param data: List of raw packages (int)
+    :return: samples, fail_count.
+             samples: List of raw samples
+             fail_count: number of samples that could not be decoded
+    """
+
     sample = None
     looking_for_pckg = 1
     samples = []
--- Scripts/Utils/record_data.py
+++ Scripts/Utils/record_data.py
@@ -1,8 +1,16 @@
 import serial
 
-def record_data(port, nsamples=-1, time_s=-1, sample_rate=44100, print_progress=True):
-    total_sample_count = -1
 
+def record_data(port, nsamples=-1, time_s=-1, sample_rate=41000, print_progress=True):
+    """
+    Record raw samples from the ADC
+    :param port: The COM port to be read from
+    :param nsamples: The number of samples to be recorded (Specify either nsamples or time_s)
+    :param time_s: The time in seconds to be recorded (Specify either nsamples or time_s)
+    :param sample_rate: Audio sample rate. (default = 41000)
+    :param print_progress: Do print percent update. (default = True)
+    :return: A list of raw samples
+    """
     if nsamples != -1 and time_s != -1:
         raise ValueError("Cannot specify both nsamples and time_s!")
     elif nsamples != -1:
@@ -10,7 +18,9 @@ def record_data(port, nsamples=-1, time_s=-1, sample_rate=44100, print_progress=
     elif time_s != -1:
         total_sample_count = 2 * sample_rate * time_s  # 2 bytes per sample
     else:
-        print_progress = False  # Do not print progress if infinite read
+        total_sample_count = -1
+        print_progress = False  # Do not print progress if indefinite read
+        raise NotImplementedError("Indefinite Recording not yet implemented! (Specify either nsamples or time_s)")
 
     index = 0
     data = []
