da81326e282c11f2d30cb1e2f8b7a18abfc929c2
TheSchilk <schilk.philipp@gmail.com>
2020-11-29 13:45:35 +0100

    Fixed typo

--- Scripts/MATLAB/Util/adc_read.m
+++ Scripts/MATLAB/Util/adc_read.m
@@ -2,7 +2,7 @@ function [audio, fail_count] = adc_read(portname, nsamples)
 %ADC_READ Read nsamples from the ADC connected to port portname
 %   Returns the collection audio samples (uint16), and the number of 
 %   packages that failed to decode. 
-    sport = serialport(portname',9600, 'Timeout', 0.1);
+    sport = serialport(portname, 9600, 'Timeout', 0.1);
     [audio, fail_count] = adc_read_openport(sport, nsamples);
     clear com;
 end
