6f0d31f137770670f3c6e5c0bc8f57376c6f726d
TheSchilk <schilk.philipp@gmail.com>
2020-12-11 15:38:41 +0100

    Lab WIP

--- Scripts/MATLAB/Characterization/ADC_FResp.m
+++ Scripts/MATLAB/Characterization/ADC_FResp.m
@@ -4,26 +4,38 @@
 % Store all? 
 % Calibrate offset?
 
-input_steps = 5:10:30000;   % Step Size?, Min/Max?
+%input_steps = logspace(0,4,20*5);   % Step Size?, Min/Max?
+input_steps = logspace(0,4,10);
 
 n_steps = size(input_steps,2);
-samples_per_step = 41000/4;  % ? Constant? Reduce for higher f's?
+samples_per_step = 41000;  % ? Constant? Reduce for higher f's?
 
 data = uint16(zeros(n_steps,samples_per_step));
+set_point = zeros(n_steps);
 
 prog_up = ProgressUpdate(n_steps,5);
 
 % Open Connection:
-com = serialport('COM8', 9600, 'Timeout', 0.1);
+com = serialport('COM6', 9600, 'Timeout', 0.1);
+
+% Instrument Connection
+FGEN = keysight_33612A('USB0::0x0957::0x4B07::MY59000369::0::INSTR'); %find usb string with tmtool
+
+% configure
+FGEN.configure_waveform(1, 'SIN','INF'); 
+FGEN.set_amplitude_value(1,4,0);
+FGEN.set_output(1,'ON');
+pause(4);
 
 % Perform Measurement:
 for step = 1:n_steps
    freq = input_steps(step); 
    
    % Set Freq.
-   % $$$$$$$$$$$$$$$$$$$$
+   FGEN.set_frequency_value(1,freq);
    
    % Read from ADC
+   adc_read_openport(com, 2100);
    [reading, fail_count] = adc_read_openport(com,samples_per_step+4);
    
    % Stop if there was a problem with package decoding:
@@ -39,11 +51,16 @@ for step = 1:n_steps
    
    % Save
    data(step,:) = reading.';
+   set_point(step) = freq;
    
    prog_up.Update(step)
 end
 
+FGEN.set_output(1,'OFF');
+FGEN.disconnect();
 clear com;
+
+save ADC_FResp.mat;
 
 % Look at: 
 %   Amplitude vs f  (look at rms?)
